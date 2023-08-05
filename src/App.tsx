ed44f920878285c328ddc3f3462771d951f654e2
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-08-05 09:49:18 -0300

    feat: app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import 'react-toastify/dist/ReactToastify.css';
+
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { ThemeProvider } from 'styled-components';
+import { Router } from './Router';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <ToastContainer theme="colored" />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
